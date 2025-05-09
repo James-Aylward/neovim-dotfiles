9f4ba7e1cf7317a89e9dd5114fd4c60b14c88ef0
James Aylward <james.michael.aylward@gmail.com>
2025-05-09 10:10:51 +1000

    sets next and prev search to center

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
 -- Moving lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
