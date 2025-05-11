7489366eae44a6a36ca63d82b331d36fdd884fe1
James Aylward <james.michael.aylward@gmail.com>
2025-05-11 11:45:00 +1000

    adds haskell language server setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -6,6 +6,7 @@ return  {
         lsp.lua_ls.setup{}
         lsp.rust_analyzer.setup{}
         lsp.texlab.setup{}
+        lsp.hls.setup{}
         lsp.svelte.setup{}
         lsp.pyright.setup{}
         lsp.java_language_server.setup{}
