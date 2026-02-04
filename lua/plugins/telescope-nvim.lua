ed2956119b7869f7cd3613d0710f5944e304dec4
James Aylward <james.aylward@powerlink.com.au>
2026-02-04 17:27:25 +1000

    adds shortcut for doc functions

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -5,5 +5,6 @@ return {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>m', function() builtin.lsp_document_symbols({ symbols = 'Function' }) end, { })
 end
 }
