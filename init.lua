b1f66b6ff5d7aea0542140c785a52012a01f8f1e
James Aylward <james.michael.aylward@gmail.com>
2026-02-07 13:28:20 +1000

    adds highlighting disable

--- init.lua
+++ init.lua
@@ -1,16 +1,26 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("options")
 require("keybinds")
 require("lazy").setup("plugins")
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "<filetype>" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+vim.o.cmdheight=0
